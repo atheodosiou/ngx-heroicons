092e2dd5eb806146eca3eef3312667758d7bf689
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2025-04-05 01:49:12 +0300

    fix: Update release.config.js

---
+++ release.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    tagPrefix: 'v',
+    packageFiles: [
+        {
+            filename: 'projects/ngx-heroicons/package.json',
+            type: 'json',
+        },
+    ],
+    bumpFiles: [
+        {
+            filename: 'projects/ngx-heroicons/package.json',
+            type: 'json',
+        },
+        {
+            filename: 'projects/ngx-heroicons/CHANGELOG.md',
+            type: 'markdown',
+        },
+    ],
+    changelogFile: 'projects/ngx-heroicons/CHANGELOG.md',
+};
